ce9e32eef8c95c8de1cb3d51a65484ea089fa409
Akademic <public@akademic.name>
2014-02-23 22:23:54 +0400

    Added die function. Added date formats.

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
     "os"
     "path/filepath"
     "log"
+    "fmt"
 )
 
 var (
     PostsDir string
     PublicDir string
     TemplatesDir string
+
+)
+
+const (
+    DateFormat     = "2006-01-02"
+    DateTimeFormat = DateFormat + " 15:04"
 )
 
 func _init() {
@@ -24,4 +31,14 @@ func _init() {
 	PostsDir = filepath.Join(pwd, "_posts")
 	TemplatesDir = filepath.Join(pwd, "_layouts")
 
+}
+
+func main() {
+    _init()
+    loadPage(PostsDir+"/test_post/index.md")
+}
+
+func die(format string, v ...interface{}) {
+    os.Stderr.WriteString(fmt.Sprintf(format+"\n", v...))
+    os.Exit(1)
 }
