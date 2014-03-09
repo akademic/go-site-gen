a25cb37e5d46cd7adc5f3ce8f511419e97713546
Akademic <public@akademic.name>
2014-03-09 18:49:31 +0400

    Post file manipulations moved into posts.go. Added moving files from post directory, to public post directory

--- posts.go
+++ posts.go
@@ -7,16 +7,77 @@ Post -> Prepare custom tags -> MarkdownToHtml -> html/template into ready file
 
 import (
     "os"
+    "io"
     "io/ioutil"
     "log"
     "path/filepath"
     "sort"
+    "strconv"
+    "strings"
+)
+
+var (
+    filesToMove []string
 )
 
 type sortablePosts []*Page
 func (s sortablePosts) Len() int           { return len(s) }
 func (s sortablePosts) Less(i, j int) bool { return s[i].PubTime.Before(s[j].PubTime) }
 func (s sortablePosts) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func genPosts() {
+
+    posts := getPosts()
+    for _, post := range posts {
+        save_dir := getPostSavePath(post)
+        save_path := filepath.Join(save_dir, "index.html")
+
+        createPost(post, save_path)
+        movePostFiles(filepath.Dir(post.Path), save_dir)
+    }
+}
+
+func movePostFiles(fromDir, toDir string) {
+    filesToMove = []string{}
+    filepath.Walk(fromDir, postDirVisit)
+
+    for _, file := range filesToMove {
+        dir, name := filepath.Split(file)
+        dir, _ = filepath.Rel(fromDir, dir)
+
+        toPath := filepath.Join(toDir, dir)
+        os.MkdirAll(toPath, 0700)
+
+        toFile := filepath.Join(toPath, name)
+
+        from, _ := os.Open(file)
+        to, _ := os.Create(toFile)
+
+        io.Copy(to, from)
+
+    }
+}
+
+//FIXME: Not tread safe
+func postDirVisit(path string, f os.FileInfo, err error) error {
+    if !f.IsDir() {
+        if f.Name() != "index.md" {
+            filesToMove = append(filesToMove, path)
+        }
+    }
+    return err
+}
+
+func getPostSavePath(post *Page) (string) {
+    parts := strings.Split(post.Path, "/")
+    name := parts[len(parts)-2 : len(parts)-1][0]
+    year := strconv.Itoa(post.PubTime.Year())
+
+    save_dir := filepath.Join(BlogDir, year, name)
+    os.MkdirAll(save_dir, 0700)
+
+    return save_dir
+}
 
 func getPosts() ([]*Page) {
 
