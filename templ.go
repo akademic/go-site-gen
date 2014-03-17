8e64f717b4ff7e3bf6bd1881c58a7205243e74ed
Akademic <public@akademic.name>
2014-03-18 00:53:26 +0400

    Added function for processing _site markdown pages

--- templ.go
+++ templ.go
@@ -6,6 +6,7 @@ import (
     "os"
     "time"
     "io/ioutil"
+    "strings"
 )
 
 type templIndex struct {
@@ -25,6 +26,10 @@ type templRss struct {
     Recent []*Post
 }
 
+type templStatic struct {
+    SiteData SiteData
+    Page *Page
+}
 
 var FuncsMap = template.FuncMap{
     "fmttime": func(t time.Time, f string) string {
@@ -82,6 +87,35 @@ func createPostRss() {
     
     f, err := os.Create( filepath.Join(PublicDir, "rss.xml") )
     
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+
+    err = t.Execute(f, tpl)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func createStaticPage(page *Page, path string, name string) {
+    tpl := new(templStatic)
+    tpl.SiteData = SiteDataVar
+    tpl.Page = page
+
+    fname := strings.Split(name, ".")[0]
+
+    save_path := filepath.Join(path, fname+".html")
+
+    main_layout := "default.html"
+    if page.Layout != "" {
+        main_layout = page.Layout
+    }
+
+    t := template.Must(template.New(main_layout).Funcs(FuncsMap).ParseFiles(filepath.Join( TemplatesDir, main_layout), filepath.Join( TemplatesDir, "static.html")) )
+
+    f, err := os.Create(save_path)
+
     if err != nil {
         panic(err)
     }
