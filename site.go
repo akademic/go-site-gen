cdb74c3104e9b7cc372395199053fd746dfa6d0d
Akademic <public@akademic.name>
2014-03-18 00:54:14 +0400

    Added processing markdown files with special way

--- site.go
+++ site.go
@@ -15,14 +15,22 @@ func siteDirVisit( path string, f os.FileInfo, err error) error {
         dir, name  := filepath.Split(path)
         dir, _ = filepath.Rel(SiteDir, dir)
 
+        ext := filepath.Ext(name)
+
         toPath := filepath.Join(PublicDir, dir)
         os.MkdirAll(toPath, 0700)
 
-        toFile := filepath.Join(toPath, name)
-        from, _ := os.Open(path)
-        to, _ := os.Create(toFile)
+        if ext == ".md" {
+            page := loadPage(path)
+            createStaticPage(page, toPath, name)
+        } else {
 
-        io.Copy(to, from)
+            toFile := filepath.Join(toPath, name)
+            from, _ := os.Open(path)
+            to, _ := os.Create(toFile)
+
+            io.Copy(to, from)
+        }
     }
 
     return err
